ba9ea008e6cddca62ef8a2f29eb58a00735ef01f
James Larson <james.larson@colostate.edu>
2025-01-20 19:12:09 -0700

    Setup parameters for 5K anomaly for prospectus figure.

--- code/src/model_analysis/exploreClimateVariability_analysis.R
+++ code/src/model_analysis/exploreClimateVariability_analysis.R
@@ -24,6 +24,10 @@ mat[1:41, 1] <- seq(1, 3, length.out = 41)   # Values from 1 to 3 by index 41
 mat[42:46, 1] <- seq(3, 1.5, length.out = 5) # Drop from 3 to 1.5 from index 41 to 46
 mat[47:81, 1] <- 1.5                        # Stay at 1.5 from index 46 to 81
 
+# Initialize an array of 81 zeros # Set the value at index 21 to be 5
+mat[1:81, 1] <- rep(0, 81)
+mat[21, 1] <- 5
+
 # Second column always 0
 mat[, 2] <- 0
 
@@ -54,7 +58,7 @@ results_list <- vector("list", num_runs)
 
 # Run the model user-defined times and store each result
 for (i in 1:num_runs) {
-  results_list[[i]] <- model(shiftingExtremes=TRUE, natvar=TRUE, historical=TRUE)
+  results_list[[i]] <- model(temperature_input=mat) #shiftingExtremes=TRUE, natvar=TRUE, historical=TRUE)
 }
 
 # Initialize a list to accumulate the sums
@@ -182,8 +186,9 @@ print(params, row.names = FALSE)
 fig = ggplot(data, aes(x = m$year)) +
     # Emissions lines
     # geom_line(aes(y = m$emissions, color = "OECD emissions"), linetype = "solid", linewidth = 0.7) +
-    geom_textline(aes(y = m$emissions, color = "OECD emissions"), label="OECD emissions", linetype = "solid", linewidth = 0.7, vjust = -0.15, size=3, hjust = 0.2)+
-    geom_textline(aes(y = m$totalemissions, color = "Total emissions"), label="Total emissions", linetype = "solid", linewidth = 0.7, vjust=-0.15, size=3, hjust=0.1) +
+    # geom_textline(aes(y = m$emissions, color = "OECD emissions"), label="OECD emissions", linetype = "solid", linewidth = 0.7, vjust = -0.15, size=3, hjust = 0.2)+
+    # geom_textline(aes(y = m$totalemissions, color = "Emissions"), label="Total emissions", linetype = "solid", linewidth = 0.7, vjust=-0.15, size=3, hjust=0.1) +
+    geom_line(aes(y = m$totalemissions, color = "Emissions"), linewidth = 0.9) +
     # geom_textline(aes(y = m$emissions_outside[,1], color = "Asia Emissions"), label="Asia emissions", linetype = "solid", linewidth = 0.7, vjust=-0.15, size=3) +
     # geom_textline(aes(y = m$emissions_outside[,2], color = "LAM Emissions"), label="LAM emissions", linetype = "solid", linewidth = 0.7, vjust=-0.15, size=3) +
     # geom_textline(aes(y = m$emissions_outside[,3], color = "MAF Emissions"), label="MAF emissions", linetype = "solid", linewidth = 0.7, vjust=-0.3, size=3) +
@@ -191,17 +196,22 @@ fig = ggplot(data, aes(x = m$year)) +
 
     # Temperature-related lines
     geom_line(aes(y = m$weather, color = "Temperature"), linewidth = 0.9) +
+    # geom_textline(aes(y = m$weather, color = "Temperature"), label="Temperature anomalies", linetype = "solid", linewidth = 0.7, vjust=-0.15, size=3) +
+
     # geom_line(aes(y = m$evidence[,1], color = "Evidence"), linewidth = 0.9) +
-    geom_line(aes(y = m$anomaly, color = "Anomaly"), linewidth = 0.9) +
+    # geom_line(aes(y = m$anomaly, color = "Anomaly"), linewidth = 0.9) +
 
     # Population distribution lines
-    geom_textline(aes(y = m$distributions[,1]/coeff, color = "Opposed"), label="Opposed", linetype = "longdash", linewidth = 0.9, vjust=-0.15, size=3, hjust=0.2) +
-    geom_textline(aes(y = m$distributions[,2]/coeff, color = "Neutral"), label="Neutral", linetype = "longdash", linewidth = 0.9, vjust=-0.15, size=3, hjust=0.2) +
-    geom_textline(aes(y = m$distributions[,3]/coeff, color = "Support"), label="Support", linetype = "longdash", linewidth = 0.9, vjust=1.5, size=3, hjust=0.2) +
+    # geom_textline(aes(y = m$distributions[,1]/coeff, color = "Opposed"), label="Opposed", linetype = "longdash", linewidth = 0.9, vjust=-0.15, size=3, hjust=0.2) +
+    # geom_textline(aes(y = m$distributions[,2]/coeff, color = "Neutral"), label="Neutral", linetype = "longdash", linewidth = 0.9, vjust=-0.15, size=3, hjust=0.2) +
+    # geom_textline(aes(y = m$distributions[,3]/coeff, color = "Support"), label="Support", linetype = "longdash", linewidth = 0.9, vjust=1.5, size=3, hjust=0.2) +
+    geom_line(aes(y = m$distributions[,1]/coeff, color = "Opposed"), label="Opposed", linewidth = 0.9) +
+    geom_line(aes(y = m$distributions[,2]/coeff, color = "Neutral"), label="Neutral", linewidth = 0.9) +
+    geom_line(aes(y = m$distributions[,3]/coeff, color = "Support"), label="Support", linewidth = 0.9) +
 
     # Adjust y-axes
     scale_y_continuous(
-      name = "Emissions, Temperature, Evidence, and Anomaly",
+      name = "Emissions and Temperature",
       limits = c(-1, 21), 
       sec.axis = sec_axis(~. * coeff, name = "Population Distributions")
     ) +
@@ -210,7 +220,7 @@ fig = ggplot(data, aes(x = m$year)) +
     labs(
       x = "Year",
       y = "Model output",
-      title = "Model Outputs over Time (2020 to 2100)"
+      # title = "Model Outputs over Time (2020 to 2100)"
     ) +
 
     # Updated color scheme using distinct, dark-pastel colors
@@ -218,11 +228,13 @@ fig = ggplot(data, aes(x = m$year)) +
       values = c(
         "OECD emissions" = "#6a040f",       # Dark-pastel red
         "Total emissions" = "#5f0f40",      # Dark-pastel purple
+        "Emissions" = "#5f0f40",      # Dark-pastel purple
         "Asia Emissions" = "#d00000",       # Dark-pastel green
         "LAM Emissions" = "#e85d04",        # Dark-pastel blue
         "MAF Emissions" = "#f48c06",        # Dark-pastel brown
         "Ref Emissions" = "#ffba08",        # Dark-pastel red
-        "Temperature" = "#85C1E9",          # Light-pastel blue
+        # "Temperature" = "#85C1E9",          # Light-pastel blue
+        "Temperature" = "#ffba08",          # Light-pastel blue
         "Evidence" = "#caf0f8",             # Light-pastel green
         "Anomaly" = "#f4acb7",              # Light-pastel pink
         # "Weather" = "#000000",              # Light-pastel pink
